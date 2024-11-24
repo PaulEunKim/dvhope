bd872b0d6fafc8d53b15afbb5060f1846d5f6fbc
Paul Kim <paul.eun.kim@gmail.com>
2024-11-24 00:48:14 -0700

    restore

--- data_loading_utils.py
+++ data_loading_utils.py
@@ -13,12 +13,10 @@ class DataLoader():
         """Returns a pandas df from all the csv files specified in the directory"""
         if not os.path.exists(self.path):
             print('Path Not Found!')
-        elif os.path.isdir(self.path):
+        else:
             dfs = [pd.read_csv(file, dtype={'FIPS':str, 'EPA Region':str}, index_col = 0) for file in self.file_paths]
 
             return pd.concat(dfs).sort_index()
-        else:
-            return pd.read_csv(path, dtype={'FIPS':str, 'EPA Region':str})
         
 if __name__ == '__main__':
     path = os.path.join('data', 'nei_pollution')
